b3e6f46dec7ff4288e420d65460cb7b2db7a8235
SerhiyDukh1984 <s.dux2011@ukr.net>
2023-04-17 18:50:15 +0300

    change app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import "./i18n";
-// import { CircularProgress } from "@mui/material";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 import { CSpinner } from "@coreui/react";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
